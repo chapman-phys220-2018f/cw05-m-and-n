c196333adc097bc5e7e56a526e10118da8b59efa
Enea Dodi <dodi@chapman.edu>
2018-10-02 15:48:00 +0000

    updated elementary.py

--- elementary.py
+++ elementary.py
@@ -1,11 +1,12 @@
-
+import scipy as sp
 class Particle(object):
     """ class """
 
-    Attributes:
-    -----------
-    data : {float : float}
-
+    mass = 0.00
+    position = (0.0, 0.0, 0.0)
+    momentum = (0.0,0.0,0.0)
+    
+'''
     def __init__(self):
         self.momentum =(px, py, pz)
 
@@ -15,6 +16,49 @@ class Particle(object):
 
     def __init__(self):
         self.mass =
+'''
 
+    def __init__(self, x, y, z):
+        self.momentum = (0.0,0.0,0.0)
+        self.mass = 1.0
+        self.position = (x,y,z)
+        
+    def impulse(self,px,py,pz):
+        momentum[0] = momentum[0] + px
+        momentum[1] = momentum[1] + px
+        momentum[2] = momentum[2] + px
+        
+    def move(self, dt):
+        #p = mv
+        #v = d/t
+        #p = (d/t)*m
+        #p = (d*m)/t
+        #p*t = d*m
+        #(p*t)/m = d
+        position[0] = position[0] + (momentum[0]*dt)/mass
+        position[1] = position[1] + (momentum[1]*dt)/mass
+        position[2] = position[2] + (momentum[2]*dt)/mass
 
-    .__init__(self, x, y, z)
+def ChargedParticle(Particle):
+    
+    charge = "neutral"
+    
+    def __init__(self, x,y,z):
+        self.momentum = (0.0,0.0,0.0)
+        self.mass = 1.0
+        self.position = (x,y,z)
+        
+def Electron(ChargedParticle):
+    
+    def __init__(self):
+        self.charge = "Negative"
+        self.mass = sp.electron_mass
+        
+def Proton(ChargedParticle):
+    
+    def __init__(self):
+        self.charge = "Positive"
+        self.mass = sp.proton_mass
+        
+        
+        
